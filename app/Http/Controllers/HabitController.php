267d2a72d9f41588e10c9cb9cb01b47cd4a5cb2f
misinayu <yama_masa22@yahoo.co.jp>
2019-02-08 12:59:16 +0000

    Create index pages

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -6,5 +6,7 @@ use Illuminate\Http\Request;
 
 class HabitController extends Controller
 {
-    //
+    public function index() {
+        return view('habits.index');
+    }
 }
---
+++ resources/views/habits/index.blade.php
@@ -0,0 +1,13 @@
+<!doctype html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>{{ config('app.name', 'HabitApp') }}</title>
+    </head>
+    <body>
+        <h1>Welcom To Laravel</h1>
+        <p>This is Laravel application habit</p>
+    </body>
+</html>
