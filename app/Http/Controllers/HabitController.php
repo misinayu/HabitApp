d134d38cdbf869cd6fed7612ae8a40ca28f52e0d
misinayu <yama_masa22@yahoo.co.jp>
2019-02-12 12:42:20 +0000

    Create app.blade.php
    common html

--- app/Http/Controllers/HabitController.php
+++ app/Http/Controllers/HabitController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 class HabitController extends Controller
 {
     public function index() {
-        return view('habits.index');
+        $title = 'Welecom To HabitApp';
+        return view('habits.index')->with('title', $title);
     }
 }
--- resources/views/habits/index.blade.php
+++ resources/views/habits/index.blade.php
@@ -1,13 +1,6 @@
-<!doctype html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-        <title>{{ config('app.name', 'HabitApp') }}</title>
-    </head>
-    <body>
-        <h1>Welcom To Laravel</h1>
-        <p>This is Laravel application habit</p>
-    </body>
-</html>
+@section('content')
+    <h1><?php echo $title; ?></h1>
+    <p>This is Laravel application habit</p>
+@endsection
