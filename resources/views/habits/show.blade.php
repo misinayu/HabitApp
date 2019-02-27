f54abefdf0a366b963492bcda51213da94fb3f3b
misinayu <yama_masa22@yahoo.co.jp>
2019-02-27 14:30:24 +0000

    tmp show

---
+++ resources/views/habits/show.blade.php
@@ -0,0 +1,12 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <h1><?php echo $title; ?></h1>
+            <p>This is Laravel application habit</p>
+        </div>
+    </div>
+</div>
+@endsection
